dc620e7b228fedba437899e59a38e8003c2f34c6
songsong'mac <cs686@126.com>
2017-12-22 14:20:17 +0800

    s

--- pwa/sw.js
+++ pwa/sw.js
@@ -8,24 +8,31 @@ self.addEventListener('install', event => {
     )
 })
 
+// Cache any new resources as they are fetched
 self.addEventListener('fetch', function(event) {
     event.respondWith(
-        caches.match(event.request)
+        caches.match(event.request, { ignoreSearch: true })
         .then(function(response) {
-            if (Response) {
-                return response
+            if (response) {
+                return response;
             }
-            var requestToCache = event.request.clone()
-            return fetch(requestToCache).then(function(response) {
-                if (!response || response.status !== 200) {
+            var fetchRequest = event.request.clone();
+
+            return fetch(fetchRequest).then(
+                function(response) {
+                    if (!response || response.status !== 200) {
+                        return response;
+                    }
+
+                    var responseToCache = response.clone();
+                    caches.open(cacheName)
+                        .then(function(cache) {
+                            cache.put(event.request, responseToCache);
+                        });
+
                     return response;
                 }
-                var responseToCache = response.clone()
-                caches.open(cacheName).then(function(cache) {
-                    cache.put(requestToCache, responseToCache)
-                })
-                return response;
-            })
+            );
         })
-    )
-})
+    );
+});
